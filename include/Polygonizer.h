4e62b8a18ebb4fd49b92cc42db155cb5ef139fba
mshooter <moira.shooter@hotmail.com>
2018-02-27 16:21:16 +0000

    think about design implementatoin

--- include/Polygonizer.h
+++ include/Polygonizer.h
@@ -2,6 +2,7 @@
 #define POLYGONIZER_H
 
 #include <vector>
+#include <QOpenGLFunctions>
 #include "glm/glm.hpp"
 #include "Datatables.h"
 
@@ -23,6 +24,15 @@ public:
     // for each cube you generate an 8 bit index this will help you to see which edge intersects with the mesh
     // the edge table will return a 12 bit number, which will tell which side of the cube intersects.
     // link with the tri table and it will tell which vertices to join
+    /// ------------------------------------------------------------------------------------------
+    /// @build get vertices, to set vertices for mesh (   std::vector<glm::vec3> m_vertices; )
+    /// ------------------------------------------------------------------------------------------
+    std::vector<glm::vec3> setVertices();
+    /// ------------------------------------------------------------------------------------------
+    /// @build get indices, to set indices for mesh (  std::vector<GLushort> m_indices; )
+    /// ------------------------------------------------------------------------------------------
+    std::vector<GLushort> setIndices();
+
 
 private:
 
--- src/Polygonizer.cpp
+++ src/Polygonizer.cpp
@@ -3,3 +3,13 @@
 Polygonizer::Polygonizer()
 {
 }
+//---------------------------------------------------------------------------------
+std::vector<glm::vec3> Polygonizer::setVertices()
+{
+    // return a vector
+}
+//---------------------------------------------------------------------------------
+std::vector<GLushort> Polygonizer::setIndices()
+{
+    // return vector
+}
